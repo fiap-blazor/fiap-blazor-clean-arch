d50ec06b0fb8111439f596fdfd6a5f858f1cd0b6
Igor Daflon <igordafloncouto@gmail.com>
2025-11-14 00:48:25 -0300

    Refatora criação de playlists e integra com backend
    Refatora a classe `PlaylistInserirRequest`, convertendo-a de `record` para `class` e adicionando novas propriedades (`Publica` e `ImagemUrl`).
    Atualiza o componente `Novo.razor` para utilizar o novo modelo, substituindo `novaPlaylist` por `Model`.
    Implementa a integração com o backend no método `SalvarPlaylist`, utilizando `_playlistsAppServico.InserirAsync`.
    Remove a exibição da propriedade `Publica` no formulário e ajusta os bindings para refletir as mudanças no modelo.
    Adiciona suporte para obter o ID do usuário autenticado via `_usuarioAutenticadoServico`.
    Realiza ajustes visuais e estruturais no formulário e corrige contadores de caracteres.
    Modifica arquivos binários do banco de dados SQLite, indicando possíveis alterações na estrutura.

--- src/Fiap.BlazorCleanArch.Aplicacao/DTOs/Requests/PlaylistInserirRequest.cs
+++ src/Fiap.BlazorCleanArch.Aplicacao/DTOs/Requests/PlaylistInserirRequest.cs
@@ -1,3 +1,9 @@
 ﻿namespace Fiap.BlazorCleanArch.Aplicacao.DTOs.Requests;
 
-public record PlaylistInserirRequest(string Nome, string Descricao);
+public class PlaylistInserirRequest
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public bool Publica { get; set; } = true;
+    public string? ImagemUrl { get; set; }
+}
